94551d3e276f4cc316b2e8aaf75f68aa48db0402
vadim <vadimnavr@gmail.com>
2016-04-15 15:57:59 +0300

    DM 4 task

---
+++ Cplusplus/SphereGame/cMultiUtuls.cpp
@@ -0,0 +1,9 @@
+//
+//  cMultiUtuls.cpp
+//  Game_Server
+//
+//  Created by Вадим Навражных on 26.03.16.
+//  Copyright © 2016 VadimAndVova. All rights reserved.
+//
+
+#include "cMultiUtuls.hpp"
---
+++ Cplusplus/SphereGame/cMultiUtuls.hpp
@@ -0,0 +1,14 @@
+//
+//  cMultiUtuls.hpp
+//  Game_Server
+//
+//  Created by Вадим Навражных on 26.03.16.
+//  Copyright © 2016 VadimAndVova. All rights reserved.
+//
+
+#ifndef cMultiUtuls_hpp
+#define cMultiUtuls_hpp
+
+#include <stdio.h>
+
+#endif /* cMultiUtuls_hpp */
